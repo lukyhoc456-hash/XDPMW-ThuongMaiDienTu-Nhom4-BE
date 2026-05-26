9fb10940da4ccddb8d644fca33f82ca023f37004
Đào Quốc Bảo <daoquocbao2k04@gmail.com>
2026-05-26 10:44:37 +0700

    update

--- app/main.py
+++ app/main.py
@@ -43,5 +43,5 @@ def get_application() -> FastAPI:
 
 
 app = get_application()
-if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
